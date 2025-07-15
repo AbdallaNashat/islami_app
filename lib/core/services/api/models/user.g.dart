f8b3636478e553f9e19303613ca48b6ca096be58
AbdallaNashat <abdonashat42@gmail.com>
2025-07-15 09:33:48 +0300

    Islami Scratch

---
+++ lib/core/services/api/models/user.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      json['id'] as String?,
+      json['name'] as String?,
+      json['avatar'] as String?,
+      json['createdAt'] as String?,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'avatar': instance.avatar,
+      'createdAt': instance.createdAt,
+    };
